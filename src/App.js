b8d56363f62f8d765cbc19fed4e8c631f76fe412
happlesful <Swislar1108@gmail.com>
2023-12-07 12:18:35 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
         className="flex flex-col justify-center items-center 
       h-screen w-screen dark:bg-slate-800 dark:text-white"
       >
-        {/*Create a scroll button/ input to select the size of the board and 
-        the player symbol*/}
-        {/*Maybe the selection of single/ multiplayer?*/}
         <TicTacToe />
       </div>
     </div>
