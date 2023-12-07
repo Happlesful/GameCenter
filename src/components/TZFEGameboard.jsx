3853341a4d1682fef95ed43190a21628223903b4
happlesful <Swislar1108@gmail.com>
2023-12-07 23:23:04 +0800

    initial commit

---
+++ src/components/TZFEGameboard.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const TZFEGameboard = () => {
+  return <div>TZFEGameboard</div>;
+};
+
+export default TZFEGameboard;
---
+++ src/components/TwoZeroFourEight.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const twoZeroFourEight = () => {
+  return <div>2048</div>;
+};
+
+export default twoZeroFourEight;
