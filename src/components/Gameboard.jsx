fda18c04795cd517e5dbbca831735575ac22b1bc
happlesful <Swislar1108@gmail.com>
2023-12-07 12:17:13 +0800

    improved customisability of game

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -7,37 +7,38 @@ const createBoard = (size) => {
   }
   return newBoard;
 };
-
+//add the size prop into the gameboard itself for selection
 const Gameboard = (Props) => {
-  const { size } = Props;
+  const { size, player1, player2 } = Props;
   const [boardSize, setBoardSize] = useState(parseInt(size));
   const [board, setBoard] = useState(createBoard(boardSize));
-  const [turn, setTurn] = useState(1);
-  const [currPlayerSymbol, setCurrentPlayerSymbol] = useState("X");
+  const [currPlayerTurn, setCurrentPlayerTurn] = useState(player1);
   const [endGame, setEndGame] = useState(false);
+  const [drawGame, setDrawGame] = useState(false);
+  const [moves, setMoves] = useState(1);
 
   useEffect(() => {
     console.log(board);
     console.log(endGame);
+    console.log(moves);
   }, [board]);
 
   const nextTurn = () => {
-    setCurrentPlayerSymbol(currPlayerSymbol === "X" ? "Y" : "X");
-    if (turn === 1) {
-      setTurn(2);
-    } else setTurn(1);
-    console.log(currPlayerSymbol);
+    setCurrentPlayerTurn(currPlayerTurn === player1 ? player2 : player1);
+    console.log(currPlayerTurn);
   };
 
   const makeMove = (row, col) => {
-    board[row][col] = currPlayerSymbol;
+    board[row][col] = currPlayerTurn;
     //make a shallow copy to update the changes or else it might not re-render
     setBoard([...board]);
     //check win after making a move on the board
+    setMoves(moves + 1);
     checkWin();
   };
 
   const checkWin = () => {
+    console.log("checking win");
     if (diagonalWin()) {
       console.log("Winner by the diagonals");
       setEndGame(true);
@@ -47,7 +48,20 @@ const Gameboard = (Props) => {
     } else if (horizontalWin()) {
       console.log("Winner by the horizontals");
       setEndGame(true);
+    } else if (checkDraw()) {
+      //display draw message and button to restart game
+      //message: no winners this game
+      console.log("No winners this round!");
+      setDrawGame(true);
     } else nextTurn();
+  };
+
+  const checkDraw = () => {
+    console.log("checking moves and size");
+    console.log(moves);
+    console.log(size ** 2);
+    if (moves >= size ** 2) return true;
+    return false;
   };
 
   const diagonalWin = () => {
@@ -56,7 +70,7 @@ const Gameboard = (Props) => {
     let secondCheck = true;
     //first check from top left to bottom right
     for (let i = 0; i < size; i++) {
-      if (board[i][i] !== currPlayerSymbol) {
+      if (board[i][i] !== currPlayerTurn) {
         firstCheck = false;
         break;
       }
@@ -65,7 +79,7 @@ const Gameboard = (Props) => {
     if (firstCheck) return true;
     //next check from top right to bottom left
     for (let i = 0; i < size; i++) {
-      if (board[size - i - 1][i] !== currPlayerSymbol) {
+      if (board[size - i - 1][i] !== currPlayerTurn) {
         secondCheck = false;
         break;
       }
@@ -79,7 +93,7 @@ const Gameboard = (Props) => {
     let winner = false;
     for (let row = 0; row < size; row++) {
       for (let col = 0; col < size; col++) {
-        if (board[row][col] !== currPlayerSymbol) {
+        if (board[row][col] !== currPlayerTurn) {
           //break to the next row since this row does not win
           break;
         } else if (col === size - 1) winner = true;
@@ -95,7 +109,7 @@ const Gameboard = (Props) => {
     let winner = false;
     for (let col = 0; col < size; col++) {
       for (let row = 0; row < size; row++) {
-        if (board[row][col] !== currPlayerSymbol) {
+        if (board[row][col] !== currPlayerTurn) {
           //break to the next row since this row does not win
           break;
         } else if (row === size - 1) winner = true;
@@ -108,14 +122,13 @@ const Gameboard = (Props) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center">
-        <span className="fixed -translate-y-32 px-2 py-1 outline-dashed">
-          <p>Players:</p>
-          <p>X @ Player 1</p>
-          <p>Y @ Player 2</p>
+        <span className="-translate-y-10 -translate-x-12 px-2 py-1 outline-dashed">
+          <p>Player 1 - {player1}</p>
+          <p>Player 2 - {player2}</p>
         </span>
         <p>Gameboard</p>
         <section>
-          <span className="flex flex-row">
+          <span className="flex flex-row pt-2">
             {board.map((row, rowIndex) => {
               return (
                 <span clasName="flex flex-col" key={rowIndex}>
@@ -136,16 +149,20 @@ const Gameboard = (Props) => {
           </span>
         </section>
       </div>
-      <span className="fixed translate-y-28 flex flex-col items-center">
-        {endGame ? (
-          <>
+      <span className="translate-y-7">
+        {drawGame || endGame ? (
+          <span className="flex flex-col items-center rounded-lg px-2 py-1 border-2 border-stone-300 bg-cyan-700">
             <p>Game over!</p>
-            <p>
-              {currPlayerSymbol} @ Player {turn} wins!
-            </p>
-          </>
+            {endGame ? (
+              <p>{currPlayerTurn} wins!</p>
+            ) : (
+              <p>No winners this round!</p>
+            )}
+          </span>
         ) : (
-          "Player " + turn + "'s turn"
+          <p className="flex flex-col items-center rounded-lg px-2 py-1 border-2 border-stone-400 bg-cyan-800">
+            {currPlayerTurn}'s turn
+          </p>
         )}
       </span>
     </div>
