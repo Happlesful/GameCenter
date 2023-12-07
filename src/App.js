3da2776d3684edce00a841ea6667d07d19c5a964
happlesful <Swislar1108@gmail.com>
2023-12-07 12:16:29 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import { Gameboard } from "./components/index";
+import { Gameboard, TicTacToe } from "./components/index";
 
 function App() {
   return (
     <div className="App">
       <div
-        className="flex justify-center items-center 
+        className="flex flex-col justify-center items-center 
       h-screen w-screen dark:bg-slate-800 dark:text-white"
       >
-        <Gameboard size="3" />
+        {/*Create a scroll button/ input to select the size of the board and 
+        the player symbol*/}
+        {/*Maybe the selection of single/ multiplayer?*/}
+        <TicTacToe />
       </div>
     </div>
   );
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import { Gameboard } from "./index";
+
+const TicTacToe = () => {
+  const [boardSize, setBoardSize] = useState("");
+  const [player1Symbol, setPlayer1Symbol] = useState("");
+  const [player2Symbol, setPlayer2Symbol] = useState("");
+  const [startGame, setStartGame] = useState(false);
+
+  const startingWindow = (
+    <div>
+      <section className="flex flex-col border-2 border-stone-400 bg-cyan-800 rounded-lg px-2 py-1">
+        <p className="text-lg text-cyan-100">Welcome to TicTacToe</p>
+        <span className="flex flex-row mt-2">
+          <p>Board size: </p>
+          <input
+            type="text"
+            placeholder="Board size"
+            onChange={(e) => {
+              if (e.target.value <= 5 && e.target.value >= 3)
+                setBoardSize(e.target.value);
+            }}
+            className="w-16 rounded-md ml-2 h-6 text-xs text-center text-black"
+          />
+        </span>
+        <span className="flex flex-row mt-2">
+          <p>Player 1 symbol: </p>
+          <input
+            type="text"
+            placeholder="P1 symbol"
+            onChange={(e) => {
+              setPlayer1Symbol(e.target.value);
+            }}
+            className="w-16 rounded-md ml-2 h-6 text-xs text-center text-black"
+          />
+        </span>
+        <span className="flex flex-row mt-2 mb-2">
+          <p>Player 2 symbol: </p>
+          <input
+            type="text"
+            placeholder="P2 symbol"
+            onChange={(e) => {
+              setPlayer2Symbol(e.target.value);
+            }}
+            className="w-16 rounded-md ml-2 h-6 text-xs text-center text-black"
+          />
+        </span>
+        <p className="text-xss w-44">^Board size should be 3 to 5</p>
+        <p className="text-xss w-44 leading-4">
+          ^Symbols should be &lt;= 5 characters
+        </p>
+        <button
+          className="rounded-md bg-stone-300 text-cyan-700 hover:text-cyan-900 hover:bg-stone-400"
+          onClick={() => {
+            if (boardSize === "") setBoardSize("3");
+            if (player1Symbol === "") setPlayer1Symbol("P1");
+            if (player2Symbol === "") setPlayer2Symbol("P2");
+            setStartGame(true);
+          }}
+        >
+          Start game
+        </button>
+      </section>
+    </div>
+  );
+
+  const gameWindow = (
+    <>
+      <button
+        className="translate-x-24 translate-y-4 rounded-md bg-indigo-200 text-slate-600 px-2 py-0.5 border-2 border-x-neutral-400 hover:bg-indigo-300 hover:border-neutral-500 hover:text-slate-900"
+        onClick={() => {
+          setStartGame(false);
+        }}
+      >
+        Restart game
+      </button>
+      <Gameboard
+        size={boardSize}
+        player1={player1Symbol}
+        player2={player2Symbol}
+      />
+    </>
+  );
+
+  return startGame ? gameWindow : startingWindow;
+};
+
+export default TicTacToe;
