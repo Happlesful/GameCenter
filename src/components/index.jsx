71f6516ef11aa959f0ae7e2489a5d637b463e626
happlesful <Swislar1108@gmail.com>
2023-12-08 15:48:47 +0800

    added 2048

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,4 @@
 export { default as TTTGameboard } from "./TTTGameboard";
 export { default as TicTacToe } from "./TicTacToe";
+export { default as TZFEGameboard } from "./TZFEGameboard";
+export { default as TwoZeroFourEight } from "./TwoZeroFourEight";
