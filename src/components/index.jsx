6d472a5aa1b9cc0135c6c908540c2bdc21d77a4e
happlesful <Swislar1108@gmail.com>
2023-12-10 15:02:41 +0800

    intial commit

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,10 +3,3 @@ export { default as TicTacToe } from "./TicTacToe";
 export { default as TZFEGameboard } from "./TZFEGameboard";
 export { default as TwoZeroFourEight } from "./TwoZeroFourEight";
 export { default as Games } from "./Games";
-
-//ToDo
-//animation
-//for mobile
-//fix illegal arrow key move in 2048
-//add colours and animations upon keyclick/ move made
-//fix game over for 2048 and stop the inf loop in find empty cell when game over
